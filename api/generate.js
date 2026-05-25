d606dab3c22457059d50ae426d457233c25a405d
xamychros-ui <xamychros@gmail.com>
2026-05-25 15:02:35 +0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,1 +1,38 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
+  try {
+    const { prompt } = req.body;
+
+    const response = await fetch(
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyC5wpeiOLWMXayc2p69AG9BPGp4D4skhvA',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [{ text: prompt }],
+            },
+          ],
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    const text =
+      data.candidates?.[0]?.content?.parts?.[0]?.text ||
+      'AI gagal generate';
+
+    res.status(200).json({ text });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+}
